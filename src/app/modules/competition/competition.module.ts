019bb6c3bd7ee47bb2d575c8cfb93ab1c16aa9b5
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-15 17:28:05 +0100

    add create competition form + add create service funtion + test creating a competition + add toastify lib

--- src/app/modules/competition/competition.module.ts
+++ src/app/modules/competition/competition.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CompetitionRoutingModule } from '@modules/competition/competition-routing.module';
 import { ComponentsModule } from '@components/components.module';
 import { CompetitionComponent } from './pages/competition/competition.component';
-import { ServicesModule } from '@app/services/services.module';
 import { CompetitionService } from '@app/services/competition/competition.service';
 import { CreateCompetitionComponent } from './pages/create-competition/create-competition.component';
 
@@ -18,6 +18,8 @@ import { CreateCompetitionComponent } from './pages/create-competition/create-co
     CommonModule,
     CompetitionRoutingModule,
     ComponentsModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [
     CompetitionService
--- src/app/modules/competition/pages/create-competition/create-competition.component.ts
+++ src/app/modules/competition/pages/create-competition/create-competition.component.ts
@@ -1,4 +1,8 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Competition } from '@app/interfaces/competition';
+import { CompetitionService } from '@app/services/competition/competition.service';
 
 @Component({
   selector: 'app-create-competition',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class CreateCompetitionComponent {
 
+  public constructor(private _service: CompetitionService) {}
+
+  public competitionForm: FormGroup = new FormGroup({
+    date: new FormControl("", Validators.required),
+    startTime: new FormControl("", Validators.required),
+    endTime: new FormControl("", Validators.required),
+    numberOfParticipants: new FormControl(10, Validators.required),
+    location: new FormControl("", Validators.required),
+    amount: new FormControl(100, Validators.required)
+  });
+
+  public submit() {
+    const competition: Competition = this.competitionForm.value
+    competition.date = formatDate(competition.date, "dd-MM-yyyy", "en-US");
+    this._service.create(competition).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log("completed");
+      }
+    });
+  } 
 }
