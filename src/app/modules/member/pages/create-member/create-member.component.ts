4ba4b8589f97e4df95200dd9ff0ba270f6e3eea3
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-18 21:15:59 +0100

    add members + add create members form

--- src/app/modules/member/pages/create-member/create-member.component.ts
+++ src/app/modules/member/pages/create-member/create-member.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Member } from '@app/interfaces/member';
+import { MemberService } from '@app/services/member/member.service';
+import { ToastService } from 'angular-toastify';
 
 @Component({
   selector: 'app-create-member',
@@ -7,4 +12,30 @@ import { Component } from '@angular/core';
 })
 export class CreateMemberComponent {
 
+  public constructor(private __service: MemberService, private __toast: ToastService, private __router: Router) {}
+
+  public formGroup: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.nullValidator]),
+    familyName: new FormControl('', [Validators.required, Validators.nullValidator]),
+    accessionDate: new FormControl('', [Validators.required, Validators.nullValidator]),
+    nationality: new FormControl('', [Validators.required, Validators.nullValidator]),
+    identityDocument: new FormControl('', [Validators.required, Validators.nullValidator]),
+    identityNumber: new FormControl('', [Validators.required, Validators.nullValidator])
+  });
+
+  public submit() {
+    if (this.formGroup.valid) {
+      this.__service.create(this.formGroup.value).subscribe({
+        next: () => {
+          this.__router.navigate(["/members"]);
+          this.__toast.success("Member Created Successfully");
+        },
+        error: () => {
+          this.__toast.error("Sorry! something went wrong");
+        }
+      });
+    }else {
+      this.__toast.warn("Please make sure all fields are valid.");
+    }
+  }
 }
